1ba1409371f733df13ef681e37f0ba21132c3e4d
Jeff Fedor <jeff.fedor@gmail.com>
2022-12-01 07:38:40 -0500

    added Posts model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+
+    if @post.valid?
+      @post.save
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).params(:body)
+  end
+end
